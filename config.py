ecd535342fc7ad77c4aebebc399230c8b83c9c2f
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-08-27 22:51:20 +0300

    change

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 
 
+# # database config
+# db_config = {
+#     'user': 'mouaid_admin',
+#     'password': 'Mn@1234',
+#     'host': '10.85.81.119',
+#     'database': 'Finance',
+#     'port': 3333
+# }
+
 # database config
 db_config = {
     'user': 'mouaid_admin',
-    'password': '0991553333',
+    'password': 'Mn@1234',
     'host': 'mysql',
     'database': 'Finance',
     'port': 3306
