547f94bc3ff6c150eb3de1fc358d6ca6a89ed080
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-09-04 14:31:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,21 +9,21 @@
 #     'port': 3333
 # }
 
-# database config tailscale
-db_config = {
-    'user': 'mouaid_admin',
-    'password': 'MnMysql1234',
-    'host': '10.159.24.164',
-    'database': 'Finance',
-    'port': 3333
-}
-
-# # database config in docker container
+# # database config tailscale
 # db_config = {
 #     'user': 'mouaid_admin',
 #     'password': 'MnMysql1234',
-#     'host': 'mysql',
+#     'host': '10.159.24.164',
 #     'database': 'Finance',
-#     'port': 3306
+#     'port': 3333
 # }
+
+# database config in docker container
+db_config = {
+    'user': 'mouaid_admin',
+    'password': 'MnMysql1234',
+    'host': 'mysql',
+    'database': 'Finance',
+    'port': 3306
+}
 
