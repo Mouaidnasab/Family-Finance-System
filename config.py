553fccf363fa846d63dd9efbf2e5a2165ed97e6f
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-09-04 14:40:37 +0300

    change

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 
-# # database config local
-# db_config = {
-#     'user': 'mouaid_admin',
-#     'password': 'MnMysql1234',
-#     'host': '192.168.0.103',
-#     'database': 'Finance',
-#     'port': 3333
-# }
+# database config local
+db_config = {
+    'user': 'mouaid_admin',
+    'password': 'MnMysql1234',
+    'host': '100.92.172.33',
+    'database': 'Finance',
+    'port': 3333
+}
 
 # # database config tailscale
 # db_config = {
@@ -18,12 +18,12 @@
 #     'port': 3333
 # }
 
-# database config in docker container
-db_config = {
-    'user': 'mouaid_admin',
-    'password': 'MnMysql1234',
-    'host': 'mysql',
-    'database': 'Finance',
-    'port': 3306
-}
+# # database config in docker container
+# db_config = {
+#     'user': 'mouaid_admin',
+#     'password': 'MnMysql1234',
+#     'host': 'mysql',
+#     'database': 'Finance',
+#     'port': 3306
+# }
 
