40abede621287e472fd4f22cb1c94ac8fbe3e349
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-09-12 17:11:39 +0300

    add sidebar

--- config.py
+++ config.py
@@ -10,20 +10,20 @@
 # }
 
 # database config tailscale
-# db_config = {
-#     'user': 'mouaid_admin',
-#     'password': 'MnMysql1234',
-#     'host': '100.92.172.33',
-#     'database': 'Finance',
-#     'port': 3333
-# }
-
-# database config in docker container
 db_config = {
     'user': 'mouaid_admin',
     'password': 'MnMysql1234',
-    'host': 'mysql',
+    'host': '100.92.172.33',
     'database': 'Finance',
-    'port': 3306
+    'port': 3333
 }
+
+# # database config in docker container
+# db_config = {
+#     'user': 'mouaid_admin',
+#     'password': 'MnMysql1234',
+#     'host': 'mysql',
+#     'database': 'Finance',
+#     'port': 3306
+# }
 
---
+++ static/js/sidebar_reports.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebarOpener = document.getElementById('sidebarOpener');
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+
+    let isSidebarOpen = false; // Sidebar is hidden by default
+
+    // Open sidebar when the opener is clicked
+    sidebarOpener.addEventListener('click', function () {
+        toggleSidebar(); // Open sidebar only when user clicks the opener
+    });
+
+    // Close sidebar when clicking outside of it (on the overlay)
+    overlay.addEventListener('click', function () {
+        closeSidebar(); // Close the sidebar when user clicks the overlay
+    });
+
+    function toggleSidebar() {
+        if (!isSidebarOpen) {
+            openSidebar(); // Only open if it's closed
+        } else {
+            closeSidebar(); // Close it if it's already open
+        }
+    }
+
+    function openSidebar() {
+        isSidebarOpen = true;
+        document.body.classList.add('sidebar-opened'); // Add class to open sidebar
+        overlay.classList.add('active');  // Show the overlay to darken the rest of the page
+    }
+
+    function closeSidebar() {
+        isSidebarOpen = false;
+        document.body.classList.remove('sidebar-opened'); // Remove class to close sidebar
+        overlay.classList.remove('active');  // Hide the overlay
+    }
+
+    // Switch between layers
+    window.showLayer = function (layerId) {
+        const layers = ['reportsLayer','mainLayer'];
+        
+        // Hide all layers first
+        layers.forEach(id => {
+            const layer = document.getElementById(id);
+            layer.style.display = 'none';  // Hide each layer
+        });
+
+        // Show the selected layer
+        const selectedLayer = document.getElementById(layerId + 'Layer');
+        if (selectedLayer) {
+            selectedLayer.style.display = 'block';  // Show the target layer
+        }
+    };
+
+    // Show dashboardLayer first by default without opening the sidebar
+    showLayer('reports');
+});
